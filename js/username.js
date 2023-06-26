9780fc944066c5734a15c67e3d86bab8ad6d1634
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-06-26 16:09:31 +0900

    Add: Username 코드 수정

--- js/username.js
+++ js/username.js
@@ -2,10 +2,14 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const HelloUserName = document.querySelector("#user-name");
+const HIDDEN_CLASSNAME = "hidden";
+
 function onLoginButtonClick(e) {
   e.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
   HelloUserName.innerText = `hello, ${userName}`;
+  HelloUserName.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginButtonClick);
