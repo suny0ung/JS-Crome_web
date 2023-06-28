5e683202166b53a4201447cf2319670ca5bc9c30
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-06-28 13:30:12 +0900

    Add: clock 기능 및 스타일

---
+++ js/clock.js
@@ -0,0 +1,20 @@
+const mainClock = document.querySelector("#clock-wrap span");
+const amPm = document.querySelector("#AM-PM");
+const secondsTime = document.querySelector("#time-seconds");
+
+function getTime() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  mainClock.innerText = `${hours}:${minutes}`;
+  secondsTime.innerText = `${seconds}`;
+  if (Number(hours) <= 12) {
+    amPm.innerText = "AM";
+  } else {
+    amPm.innerText = "PM";
+  }
+}
+
+setInterval(getTime, 1000);
